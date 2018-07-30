65f909afdbff1a183781f6d1c8b7f1db26c46093
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-07-30 19:55:33 +0200

    Sidebar cleanup

--- client/src/modules/sidebar/Sidebar.js
+++ client/src/modules/sidebar/Sidebar.js
@@ -10,13 +10,36 @@ const Sidebar = ({isAdmin}) => {
 		<section className="sidebar">
 		<input id="toggleSidebar" className="btn btn-primary" type="checkbox" />
 			<div className="sidebar__nav list-group">
-				<NavLink to="/board" activeClassName="sidebar__link--active" className="sidebar__link list-group-item list-group-item-action"><FontAwesomeIcon icon={faColumns} /> <p className="sidebar__text">Current Board</p></NavLink>
-				<NavLink to="/create-board" activeClassName="sidebar__link--active" className="sidebar__link list-group-item list-group-item-action"><FontAwesomeIcon icon={faPlus} /> <p className="sidebar__text">Create Board</p></NavLink>
-				<NavLink to="/board-editor" activeClassName="sidebar__link--active" className={`sidebar__link list-group-item list-group-item-action${isAdmin ? '' :' disabled'}`}><FontAwesomeIcon icon={faEdit} /> <p className="sidebar__text">Board Editor</p></NavLink>
-				<NavLink to="/analytics" activeClassName="sidebar__link--active" className="sidebar__link list-group-item list-group-item-action"><FontAwesomeIcon icon={faChartPie} /> <p className="sidebar__text">Analytics</p></NavLink>
-				<NavLink to="/calendar" activeClassName="sidebar__link--active" className="sidebar__link list-group-item list-group-item-action"><FontAwesomeIcon icon={faCalendarAlt} /> <p className="sidebar__text">Calendar</p></NavLink>
-				<NavLink to="/team" activeClassName="sidebar__link--active" className={`sidebar__link list-group-item list-group-item-action${isAdmin ? '' :' disabled'}`}><FontAwesomeIcon icon={faUsers} /> <p className="sidebar__text">Team</p></NavLink>
-				<NavLink to="/settings" activeClassName="sidebar__link--active" className="sidebar__link list-group-item list-group-item-action"><FontAwesomeIcon icon={faCogs} /> <p className="sidebar__text">Settings</p></NavLink>
+				<NavLink to="/board"
+								 activeClassName="sidebar__link--active"
+								 className="sidebar__link list-group-item list-group-item-action"
+				><FontAwesomeIcon icon={faColumns} /><p className="sidebar__text">Current Board</p>
+				</NavLink>
+				<NavLink to="/create-board"
+								 activeClassName="sidebar__link--active"
+								 className="sidebar__link list-group-item list-group-item-action"
+				><FontAwesomeIcon icon={faPlus} /><p className="sidebar__text">Create Board</p>
+				</NavLink>
+				<NavLink to="/board-editor"
+								 activeClassName="sidebar__link--active"
+								 className={`sidebar__link list-group-item list-group-item-action${isAdmin ? '' :' disabled'}`}
+				><FontAwesomeIcon icon={faEdit} /><p className="sidebar__text">Board Editor</p>
+				</NavLink>
+				<NavLink to="/analytics"
+								 activeClassName="sidebar__link--active"
+								 className="sidebar__link list-group-item list-group-item-action"
+				><FontAwesomeIcon icon={faChartPie} /><p className="sidebar__text">Analytics</p>
+				</NavLink>
+				<NavLink to="/calendar"
+								 activeClassName="sidebar__link--active"
+								 className="sidebar__link list-group-item list-group-item-action"
+				><FontAwesomeIcon icon={faCalendarAlt} /><p className="sidebar__text">Calendar</p>
+				</NavLink>
+				<NavLink to="/team"
+								 activeClassName="sidebar__link--active"
+								 className={`sidebar__link list-group-item list-group-item-action${isAdmin ? '' :' disabled'}`}
+				><FontAwesomeIcon icon={faUsers} /><p className="sidebar__text">Team</p>
+				</NavLink>
 			</div>
 		</section>
 		</Fragment>
